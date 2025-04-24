17af4c5deb32923a833f6ac0f29a4b69d659606e
Jacob E Beverforden <jbeverforden@students.avc.edu>
2025-04-24 13:22:17 -0700

    updated main.hpp

--- main.hpp
+++ main.hpp
@@ -1,8 +1,53 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+int getTwoValues(int, int);
+int getNextPrime(int);
+int getPrevPrime(int);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+int getTwoValues(int begin, int end)
+{
+    do
+    {
+        cin >> begin >> end;
+    } while (begin >= end);
+}
+
+int getNextPrime(int begin)
+{
+    int i, j;
+    for (i = begin + 1; i < 10000; i++)
+    {
+        for (j = 2; j < 10000; j++)
+        {
+            if (i % j == 0)
+            {
+                break;
+            }
+        }
+            if (i == j)
+            {
+                return j;
+            }
+    }
+}
+
+int getPrevPrime(int end)
+{
+    int i, j;
+    for (i = end - 1; i > -10000; i--)
+    {
+        for (j = end; j > -10000 ;j--)
+        {
+            if (i % j == 0)
+            {
+                break;
+            }
+
+        }
+             if (i == j)
+            {
+                return j;
+            }
+    }
+}
